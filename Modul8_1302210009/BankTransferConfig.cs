33c97085ed69a8d6df6fb13d6a7a3437fd0c2e58
heruiksan <heruiksan43@gmail.com>
2023-04-12 17:20:59 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ Modul8_1302210009/BankTransferConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul8_1302210009
+{
+    internal class BankTransferConfig
+    {
+    }
+}
--- Modul8_1302210009/Program.cs
+++ Modul8_1302210009/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using Modul8_1302210009;
+using System.Runtime.Intrinsics.X86;
+
+internal class program
+{
+    private static void Main(string[] args)
+    {
+        BankTransferConfig b = new BankTransferConfig();
+
+        if (b.config.lang.Equals("en"))
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+        }else if (b.config.lang.Equals("id"))
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+        }
+
+        string transfer = Console.ReadLine();
+        int jumTransfer = (int)Convert.ToInt32(transfer);
+
+
+        int FEEtransfer = 0;
+        if(jumTransfer<= b.config.transfer.threshold)
+        {
+            FEEtransfer = b.config.transfer.low_fee;
+        }else if(jumTransfer >= b.config.transfer.threshold)
+        {
+            FEEtransfer = b.config.transfer.high_fee;
+        }
+
+
+    }
+}
+
